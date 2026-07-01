03933fe30d5124c464a4d9fc5dcac779d597ddc9
barpyfartz <guardiantvzet@gmail.com>
2026-07-01 06:53:37 +0200

    bl xrefs

--- mem.h
+++ mem.h
@@ -8,10 +8,12 @@ namespace mem {
     extern size_t size;
     extern size_t text_start;
     extern size_t text_end;
+    extern bool is_arm64;
     bool open(const char* path);
     void close();
     uintptr_t find_str(const char* str);
     uintptr_t find_bytes(const char* sig);
     std::vector<uintptr_t> find_xrefs(uintptr_t target);
+    std::vector<uintptr_t> find_bl_xrefs(uintptr_t target);
     uintptr_t find_func(uintptr_t from);
 }
